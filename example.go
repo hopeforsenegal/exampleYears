005136ef8ca1b8c5f86a4b1edc58005117fc6d5f
Kamau Vassall <kamau.vassall@gmail.com>
2016-11-22 18:00:29 -0600

    Adding comments

--- example.go
+++ example.go
@@ -13,6 +13,7 @@ import (
 func HighestYear(yearStrings []string) (maxYear int, numberAlive int) {
 	m := make(map[int]int)
 
+	// Mark the begging and ends within the map
 	for _, v := range yearStrings {
 		yearRange := strings.Split(v, ":")
 		start, _ := strconv.Atoi(yearRange[0])
@@ -21,12 +22,14 @@ func HighestYear(yearStrings []string) (maxYear int, numberAlive int) {
 		m[end]--
 	}
 
+	// sort the keys so we can access the map sequentially according to time
 	si := make([]int, 0, len(m))
 	for i := range m {
 		si = append(si, i)
 	}
 	sort.Ints(si)
 
+	// go through the map and when we come to any end we see if we've increased the number alive
 	counter := 0
 	for _, key := range si {
 		value := m[key]
@@ -43,6 +46,7 @@ func HighestYear(yearStrings []string) (maxYear int, numberAlive int) {
 }
 
 func YearList(filepath string) []string {
+	// open the file safely
 	file, err := os.Open(filepath)
 	if err != nil {
 		fmt.Printf("\ndataset: '%s' doesn't exist \n", filepath)
@@ -52,6 +56,7 @@ func YearList(filepath string) []string {
 
 	fmt.Printf("\ndataset: '%s'\n", filepath)
 
+	// retrieve the lines for begin and end dates
 	var yearList []string
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
@@ -68,7 +73,9 @@ func YearList(filepath string) []string {
 }
 
 func HighestYearTest(filepath string) {
+	// obtain the dataset
 	if yearList := YearList(filepath); yearList != nil {
+		// find the year with the most people alive in the dataset
 		highestYear, count := HighestYear(yearList)
 		fmt.Printf("The year with the most number of people alive for dataset %s is %v with %v people alive\n", filepath, highestYear, count)
 	}
